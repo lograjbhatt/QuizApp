5631aaebcde570b2a717f4250f781966b2658626
itosh <itosh@ITO_ASUS>
2024-02-19 08:50:47 +0545

    Add project files.

---
+++ QuizApp/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace QuizApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime EnrolledDate { get; set; }
+        public string? FullAddress { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
